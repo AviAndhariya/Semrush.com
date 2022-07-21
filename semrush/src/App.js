17153f5690a61477696fd7cb11e61fee0962f985
AviAndhariya <avi.andhariya@gmail.com>
2022-07-22 02:16:55 +0530

    day-3 task homeTop

--- semrush/src/App.js
+++ semrush/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import HomeTop from './Components/HomeTop';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     {/* <Footer/> */}
+     <HomeTop />
+     <Footer/>
     </div>
   );
 }
