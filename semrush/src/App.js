723494df5e5a782f2ed3944738716a59f6d2d1a3
AviAndhariya <avi.andhariya@gmail.com>
2022-07-23 02:54:16 +0530

    slider added

--- semrush/src/App.js
+++ semrush/src/App.js
@@ -1,7 +1,12 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import 'react-tabs/style/react-tabs.css';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import HomeTop from './Components/HomeTop';
+import Login from "./Components/Login";
+
 
 function App() {
   return (
@@ -9,6 +14,7 @@ function App() {
      <Navbar/>
      <HomeTop />
      <Footer/>
+     {/* <Login /> */}
     </div>
   );
 }
